ec47aaef9f6a962e3402d7c0bffe37fc837bd568
teshager21 <teshager8922@gmail.com>
2020-06-12 09:46:53 -0600

    select with multiple order criteria

--- SELECT/SELECT_from_noble.sql
+++ SELECT/SELECT_from_noble.sql
@@ -74,3 +74,9 @@ WHERE winner= "eugene o\'neill"
 SELECT winner, yr, subject
 FROM nobel
 WHERE winner REGEXP "^Sir" ORDER BY yr DESC
+
+--order and exclude
+SELECT winner, subject
+  FROM nobel
+ WHERE yr=1984
+ ORDER BY subject IN ('Physics','Chemistry'),subject,winner
