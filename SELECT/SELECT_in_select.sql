041aeaee4fabfca41e9dd8dda3bb17094f8a0b70
teshager21 <teshager8922@gmail.com>
2020-06-12 10:05:43 -0600

    select multilayer multirow

--- SELECT/SELECT_in_select.sql
+++ SELECT/SELECT_in_select.sql
@@ -9,3 +9,12 @@ WHERE continent="europe" AND GDP/population >
                (SELECT GDP/population
                FROM world
                WHERE name= "united kingdom")
+
+ --select multilayer with IN
+ SELECT name, continent
+FROM world
+WHERE continent IN
+ (SELECT continent
+   FROM world
+   WHERE  name='argentina' or name='australia')
+ORDER BY name
