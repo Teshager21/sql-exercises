32ed402a836e470d1e59fda0bea7f22961b6585d
teshager21 <teshager8922@gmail.com>
2020-06-12 01:59:49 -0600

    select the smallest countries

--- SELECT/SELECT_from_world.sql
+++ SELECT/SELECT_from_world.sql
@@ -5,3 +5,7 @@ SELECT population FROM world
 
   SELECT name, population FROM world
   WHERE name IN ('sweden', 'norway', 'denmark');
+
+  --which countries are too small
+  SELECT name, area FROM world
+  WHERE area BETWEEN 200000 AND 250000
