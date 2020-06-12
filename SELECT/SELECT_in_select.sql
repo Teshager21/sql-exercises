384c9c6d9a940a6755f5d6d931fce4c49fc3a544
teshager21 <teshager8922@gmail.com>
2020-06-12 09:57:55 -0600

    select multilayer multi-criterion

--- SELECT/SELECT_in_select.sql
+++ SELECT/SELECT_in_select.sql
@@ -2,3 +2,10 @@ SELECT name FROM world
   WHERE population >
      (SELECT population FROM world
       WHERE name='russia')
+ --select multi criteria with multilayer
+ SELECT name 
+FROM world
+WHERE continent="europe" AND GDP/population >
+               (SELECT GDP/population
+               FROM world
+               WHERE name= "united kingdom")
