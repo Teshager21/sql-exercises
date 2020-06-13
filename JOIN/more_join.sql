6a09ae36e47b05399cb826832946c41a885d1531
teshager21 <teshager8922@gmail.com>
2020-06-13 05:12:49 -0600

    join three tables

--- JOIN/more_join.sql
+++ JOIN/more_join.sql
@@ -56,3 +56,9 @@ FROM movie JOIN casting ON id=movieid
 WHERE id IN (SELECT movieid 
 FROM casting JOIN actor ON id=actorid
 WHERE actor.name="harrison ford" AND casting.ord!=1)
+
+--Join three tables
+
+SELECT  movie.title, actor.name
+FROM movie JOIN casting ON movie.id=casting.movieid JOIN actor ON actor.id=casting.actorid
+WHERE movie.yr=1962 AND casting.ord=1
